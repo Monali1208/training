b7814c2d73fbff736e22daa6986478862b1dda19
Mona <mona@gmail.com>
2022-09-01 10:22:02 -0400

    Forloop and if condition practice

---
+++ src/training/OddEvenNumbers.java
@@ -0,0 +1,22 @@
+package training;
+
+public class OddEvenNumbers {
+
+	public static void main(String[] args) {
+		
+		
+		
+		for (int j = 1; j <=10; j++) {
+			if (j%2==0)
+			System.out.println(j+"  it's even number");
+			else
+				System.out.println(j+ "  it's odd number");
+	
+			
+		}
+		
+		
+
+	}
+
+}
